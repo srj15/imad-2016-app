a9f3beec99c7662bffe671f57b184c17533676b5
srj15 <gsooraj15@gmail.com>
2016-11-13 00:13:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }));
 
 function createTemplate (data) {
